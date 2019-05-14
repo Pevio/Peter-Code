8485e1c84a47132dcaaf8c494dbda32598fcfcd6
KingPeter1024 <47261552+KingPeter1024@users.noreply.github.com>
2019-05-14 16:42:44 -0500

    Update Spyfall.java

--- Spyfall/Spyfall.java
+++ Spyfall/Spyfall.java
@@ -1,9 +1,6 @@
-//GUI Spyfall in Java, uses Locations.dat and Spyfall Settings.dat
-
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-//import javafx.scene.image.Image;
 
 import javafx.scene.text.Font;
 
@@ -15,17 +12,21 @@ import javafx.scene.control.*;
 import java.io.*;
 import java.util.Scanner;
 
-public class Spyfall extends Application {
-	static boolean isStarted;
-	static int numLocations;
-	static String[] locationNames = new String[100];
-	static String[][] roles = new String [100][8];
-	static int numPlayers;
-	static String playerRoles[] = new String[8];
-	static int currentLocation;
+/* Provides functionality to play Spyfall on a computer.  Uses the locations.dat file to input all locations, and if Spyfall Settings.dat
+ * exists, uses it to store the names of the people playing.
+ */
 
-	TextField[] names = new TextField[8];
-	CheckBox chkRoles = new CheckBox("Roles");
+public class Spyfall extends Application {
+	private boolean isStarted;
+	private int numLocations;
+	private String[] locationNames = new String[100];
+	private String[][] roles = new String [100][8];
+	private int numPlayers;
+	private String playerRoles[] = new String[8];
+	private int currentLocation;
+
+	private TextField[] names = new TextField[8];
+	private CheckBox chkRoles = new CheckBox("Roles");
 
 	public static void main(String[] args) {
 		launch(args);
@@ -34,12 +35,13 @@ public class Spyfall extends Application {
 	public void start(Stage primaryStage) {
 		isStarted = false;
 		Pane pane = new Pane();
-
+		
 		//VBox for setting up a new game
 		VBox newGame = new VBox(5);
 
 		for (int i = 0; i < 8; i++) {
 			names[i] = new TextField("");
+			names[i].setMinWidth(150);
 			newGame.getChildren().add(names[i]);
 		}
 		Button startGame = new Button("Start Game");
@@ -48,13 +50,14 @@ public class Spyfall extends Application {
 		seeLocations.setOnAction(e -> locationsClick());
 		pane.getChildren().add(newGame);
 		newGame.relocate(20, 20);
-
+		
 		//VBox for things in a current game
 		VBox currentGame = new VBox(5);
 		Button[] seeLocation = new Button[8];
 		for (int i = 0; i < 8; i++) {
 			seeLocation[i] = new Button();
 			seeLocation[i].setVisible(false);
+			seeLocation[i].setMinWidth(150);
 			final int fat = i;
 			seeLocation[i].setOnAction(e -> seeLocation(fat, seeLocation[fat]));
 			currentGame.getChildren().add(seeLocation[i]);
@@ -68,7 +71,8 @@ public class Spyfall extends Application {
 
 		startGame.setOnAction(e -> startGame(seeLocation, finishGame));
 		finishGame.setOnAction(e -> finishGame(seeLocation, finishGame));
-
+		
+		//Load locations, and if we can't, exit
 		try {
 			loadLocations();
 		} catch (Exception e) {
@@ -78,10 +82,10 @@ public class Spyfall extends Application {
 			System.out.println("Error loading locations.");
 			System.exit(0);
 		}
-
-		Scene scene = new Scene(pane, 370, 320);
+		
+		//Finish up
+		Scene scene = new Scene(pane, 370, 390);
 		primaryStage.setScene(scene);
-		//primaryStage.getIcons().add(new Image("icon.jpg"));
 		primaryStage.setTitle("Spyfall");
 		primaryStage.show();
 	}
@@ -165,7 +169,6 @@ public class Spyfall extends Application {
 			Scene scene = new Scene(pane, 180, 110);
 			Stage newStage = new Stage();
 			newStage.setScene(scene);
-			//newStage.getIcons().add(new Image("icon.jpg"));
 			newStage.setTitle("");
 			newStage.show();
 			name.setDisable(true);
@@ -174,6 +177,7 @@ public class Spyfall extends Application {
 
 	public void finishGame(Button[] seeLocation, Button finishGame) {
 		if (isStarted) {
+			//Reveal all information and enable a new game to start
 			for (int i = 0; i < numPlayers; i++) {
 				seeLocation[i].setText(seeLocation[i].getText() + ": " + playerRoles[i]);
 				seeLocation[i].setDisable(false);
@@ -184,6 +188,7 @@ public class Spyfall extends Application {
 	}
 
 	public void loadLocations() throws Exception {
+		//Loads the locations from the saved file
 		File file = new File("Locations.dat");
 		Scanner input = new Scanner(file);
 
@@ -219,6 +224,7 @@ public class Spyfall extends Application {
 
 	@Override
 	public void stop() throws Exception {
+		//When we close, save all settings
 		File file = new File("Spyfall Settings.dat");
 		PrintWriter writer = new PrintWriter(file);
 
