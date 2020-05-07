462be03762e51cf0ffd77d3790c6502ccdf5e1b1
KingPeter1024 <47261552+KingPeter1024@users.noreply.github.com>
2020-05-06 20:16:47 -0500

    Update LBL.java

--- FX_API/LBL.java
+++ FX_API/LBL.java
@@ -1,26 +1,26 @@
 package fx;
 
 import javafx.geometry.Pos;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
-public class LBL extends Label {
+public class BTN extends Button {
 	
-	public LBL() {
+	public BTN() {
 		this("");
 	}
-	public LBL(String text) {
+	public BTN(String text) {
 		this(text, FX.DEFAULT_COLOR, FX.DEFAULT_FONT);
 	}
-	public LBL(String text, Color color) {
+	public BTN(String text, Color color) {
 		this(text, color, FX.DEFAULT_FONT);
 	}
-	public LBL(String text, double font) {
+	public BTN(String text, double font) {
 		this(text, FX.DEFAULT_COLOR, font);
 	}
-	public LBL(String text, Color color, double font) {
+	public BTN(String text, Color color, double font) {
 		super(text);
 		setFont(font, false);
 		this.setTextFill(color);
@@ -37,7 +37,7 @@ public class LBL extends Label {
 		FX.makeDim(this, width, height);
 	}
 	
-	public void setFont(double size, boolean bold) {
+	public void setFont(double size, boolean bold ) {
 		setFont(Font.font(null, bold ? FontWeight.BOLD : FontWeight.NORMAL, size));
 	}
 	public void setBold(boolean value) {
@@ -45,12 +45,5 @@ public class LBL extends Label {
 	}
 	public void setFont(double value) {
 		this.setFont(new Font(value));
-	}
-	
-	public void addLine(String text) {
-		FX.addLine(this, text);
-	}
-	public void addText(String text) {
-		FX.addText(this, text);
 	}
 }
